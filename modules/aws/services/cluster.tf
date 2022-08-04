aeb579e35bd64555c1458496dd5a238085706a6f
jpolchlo <jpolchlopek@azavea.com>
2022-08-04 14:09:48 -0400

    Remove services module; content has been moved to a deployment stage

--- modules/aws/services/cluster.tf
+++
@@ -1,4 +0,0 @@
-module "eks" {
-  source = "../cluster"
-  cluster_name = local.cluster_name
-}
--- modules/aws/services/locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  cluster_name = "${var.app_name}-${var.environment}"
-
-  tags = {
-    Name    = var.app_name
-    GithubRepo = var.repo_name
-    GithubOrg  = "azavea"
-  }
-}
